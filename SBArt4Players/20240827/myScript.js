7225471812faf8fffc72a09da5b3291b195d5434
Tatsuo Unemi <unemi@iss.soka.ac.jp>
2024-08-28 15:04:35 +0900

    SBArtPlayers revised

--- SBArt4Players/20240827/myScript.js
+++ SBArt4Players/20240827/myScript.js
@@ -1,6 +1,9 @@
-var canvas, ifrm, playBtn
+// Javascript code for SBArt animation player
+// (C) Tatsuo Unemi, 2024.
+var canvas, playBtn
 var gl, program, vshader, shader1, shader2, audio1, audio2, codeNumb = 1
 var readyToDraw = false, tPos, timer
+var canvasW, canvasH, isWebKit
 const nCodes = 20
 function makeShader(element, shaderType) {
 	const shader = gl.createShader(shaderType)
@@ -11,43 +14,17 @@ function makeShader(element, shaderType) {
 		return null
 	} else return shader
 }
-function preload() {
+function preload(postProc) {
 	audio2 = new Audio(codeNumb + ".mp3")
-	ifrm.src = codeNumb + ".txt"
+	if (postProc) audio2.onloadeddata = postProc
+	shader2 = makeShader(document.getElementById("fs" + codeNumb), gl.FRAGMENT_SHADER)
 }
-function setup() {
-	canvas = document.querySelector("canvas")
-	gl = canvas.getContext("webgl")
-	if (!gl) alert("Could not get WebGL context.")
-	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
-	gl.bufferData(gl.ARRAY_BUFFER,
-		new Float32Array([-1,-1,0, 1,-1,0, -1,1,0, 1,1,0]), gl.STATIC_DRAW)
-	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
-	gl.enableVertexAttribArray(0)
+function canvasSizeSetup() {
 	gl.viewport(0, 0, canvas.width, canvas.height)
-	playBtn = document.querySelector("img")
-	canvas.onfullscreenchange = (e)=>{
-		if (document.fullscreenElement) {
-//			canvas.style.width = screen.width + "px"
-//			canvas.style.height = screen.height + "px"
-			playBtn.hidden = true
-			playNext()
-		} else {
-//			canvas.style.width = "1280px"
-//			canvas.style.height = "720px"
-			audio1.pause()
-			clearInterval(timer)
-			playBtn.hidden = false
-		}
-//		canvas.width = screen.width * devicePixelRatio
-//		canvas.height = screen.height * devicePixelRatio
-		
+	if (program) {
+		gl.uniform1f(gl.getUniformLocation(program, "xScale"), canvas.width/canvas.height)
+		gl.uniform2f(gl.getUniformLocation(program, "size"), canvas.width, canvas.height)
 	}
-	vshader = makeShader(document.getElementById("vshader"), gl.VERTEX_SHADER)
-	ifrm = document.querySelector("iframe")
-	ifrm.onload = ()=>{
-		shader2 = makeShader(ifrm.contentDocument.body, gl.FRAGMENT_SHADER) }
-	preload()
 }
 async function drawScene() {
 	if (!readyToDraw) return
@@ -55,9 +32,47 @@ async function drawScene() {
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
 	gl.flush()
 }
+function didCanvasFullScreenChange() {
+	let stw, sth, postProc
+	if (document.fullscreenElement) {
+		stw = screen.width; sth = screen.height
+		playBtn.hidden = true
+		postProc = ()=>{ if (audio1) audio1.play(); else playNext() }
+	} else {
+		stw = canvasW; sth = canvasH
+		playBtn.hidden = false
+		postProc = ()=>{ audio1.pause(); clearInterval(timer); drawScene() }
+	}
+	canvas.style.width = stw + "px"
+	canvas.style.height = sth + "px"
+	canvas.width = stw * devicePixelRatio
+	canvas.height = sth * devicePixelRatio
+	canvasSizeSetup()
+	postProc()
+}
+function setup() {
+	canvas = document.querySelector("canvas")
+	canvasW = canvas.width
+	canvasH = canvas.height
+	gl = canvas.getContext("webgl")
+	if (!gl) alert("Could not get WebGL context.")
+	gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+	gl.bufferData(gl.ARRAY_BUFFER,
+		new Float32Array([-1,-1,0, 1,-1,0, -1,1,0, 1,1,0]), gl.STATIC_DRAW)
+	gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0)
+	gl.enableVertexAttribArray(0)
+	playBtn = document.querySelector("svg")
+	playBtn.hidden = true
+	canvas.addEventListener((navigator.userAgent.indexOf("AppleWebKit") >= 0)?
+		"webkitfullscreenchange" : "fullscreenchange",
+		didCanvasFullScreenChange)
+	vshader = makeShader(document.getElementById("vshader"), gl.VERTEX_SHADER)
+	preload(()=>{ playBtn.hidden = false })
+}
 function startPlay() {
     if (canvas.requestFullScreen) canvas.requestFullScreen()
     else if (canvas.webkitRequestFullScreen) canvas.webkitRequestFullScreen()
+    else if (canvas.mozRequestFullScreen) canvas.mozRequestFullScreen()
     else playNext()
 	timer = setInterval(drawScene, 1000./60.)
 }
@@ -74,8 +89,7 @@ function playNext() {
 	if (!gl.getProgramParameter(program, gl.LINK_STATUS))
 		{ alert("Could not Link shaders"); return }
 	gl.useProgram(program)
-	gl.uniform1f(gl.getUniformLocation(program, "xScale"), canvas.width/canvas.height)
-	gl.uniform2f(gl.getUniformLocation(program, "size"), canvas.width, canvas.height)
+	canvasSizeSetup()
 	tPos = gl.getUniformLocation(program, "T")
 	audio1 = audio2
 	audio1.addEventListener("ended", playNext)
---
+++ SBArt4Players/makeHTML.sh
@@ -0,0 +1,52 @@
+#! /bin/zsh
+# (C) Tatsuo Unemi, August 28, 2024.
+# Makes MP3 files from CAF
+if [ -f 1.caf ]
+then for x in {[1-9],[1-9][0-9]}.caf
+do ffmpeg -i $x -v fatal -y `echo $x | cut -d. -f1`.mp3
+rm $x
+done
+elif [ ! -f 1.mp4 ]
+then echo "No sound file found in `cwd`."; exit
+fi
+# makes index.html unified the shader codes extracted from HTML files.
+if [ ! -f 1.html ]; then echo "No HTML file found in `cwd`."; exit; fi
+cat > index.html <<EOF
+<!doctype html>
+<!-- (C) Tatsuo Unemi, `date +%Y` -->
+<html lang="en-US">
+<head>
+<meta charset="utf-8"/>
+<title>SBArt4 Animation on WebGL `date +%Y%m%d`</title>
+<script src="myScript.js" type="text/javascript"></script>
+<script id="vshader" type="x-shader/x-vertex">
+attribute vec4 vPos;
+uniform float xScale;
+varying vec2 pCoord;
+void main(void) {
+	gl_Position = vPos;
+	pCoord = vec2(vPos.x*xScale,vPos.y);
+}
+</script>
+EOF
+for x in {[1-9],[1-9][0-9]}.html
+do echo '<script id="fs'`echo $x | cut -d. -f1`'" type="x-shader/x-fragment">' >> index.html
+awk '/script>/{m=0}m==1{print}/fshader/{m=1}' $x >> index.html
+echo '</script>' >> index.html
+rm $x
+done
+cat >> index.html <<EOF
+</head>
+<body onload="setup()">
+<canvas width=1280px height=720px></canvas>
+<svg onclick="startPlay(this)" style="position:absolute;top:30%;left:40%"
+version="1.1" width="256.0" height="256.0" viewBox="0 0 256 256">
+<path d="M48,240C30.327,240 16,225.673 16,208L16,48C16,30.327 30.327,16 48,16
+L208,16C225.673,16 240,30.327 240,48L240,208C240,225.673 225.673,240 208,240Z"
+fill="#000000" fill-opacity="0.5" stroke="none"></path>
+<path d="M63.500,47.500L63.500,207.500L208,128Z"
+fill="#FEFEFE" fill-opacity="0.8" stroke="none"></path>
+</svg>
+</body>
+</html>
+EOF
